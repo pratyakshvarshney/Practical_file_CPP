a62063a98ae3b35682adf36a6ac51c7528d7be1f
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-10-15 08:17:23 +0530

    Create copy_constructor.cpp

---
+++ copy_constructor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+    private:
+        string color;
+        string brand;
+        double price;
+
+    public:
+        // Parameterized constructor
+        Car(string c, string b, double p) {
+            color = c;
+            brand = b;
+            price = p;
+        }
+
+        // Copy constructor
+        Car(const Car &c) {
+            color = c.color;
+            brand = c.brand;
+            price = c.price;
+        }
+
+        // Method to display car details
+        void display() {
+            cout << "Color: " << color << endl;
+            cout << "Brand: " << brand << endl;
+            cout << "Price: $" << price << endl;
+        }
+};
+
+int main() {
+    // Create an instance of Car
+    Car car1("Red", "Toyota", 20000.0);
+
+    // Use the copy constructor to create a new instance
+    Car car2 = car1;
+
+    cout << "Details of car 1:" << endl;
+    car1.display();
+
+    cout << "Details of car 2 (copy of car 1):" << endl;
+    car2.display();
+
+    return 0;
+}
