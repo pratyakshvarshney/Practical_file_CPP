1a28c3a8730cd5c7f5f46baedc73dce85b84b57e
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-10-15 12:27:45 +0530

    Create Q7.cpp

---
+++ Q7.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+// Base class: Person
+class Person {
+    protected:
+        string name;
+        int age;
+    public:
+        // Constructor for Person class
+        Person(string n, int a) : name(n), age(a) {
+            cout << "Person constructor called for: " << name << endl;
+        }
+        
+        // Method to display person details
+        void showPersonDetails() const {
+            cout << "Name: " << name << endl;
+            cout << "Age: " << age << endl;
+        }
+        
+        // Destructor for Person class
+        ~Person() {
+            cout << "Person destructor called for: " << name << endl;
+        }
+};
+
+// Derived class: Employee (inherits from Person)
+class Employee : public Person {
+    private:
+        float salary;
+    public:
+        // Constructor for Employee class
+        Employee(string n, int a, float s) : Person(n, a), salary(s) {
+            cout << "Employee constructor called for: " << name << endl;
+        }
+        
+        // Method to display employee details
+        void showEmployeeDetails() const {
+            showPersonDetails(); // Calls base class method to display name and age
+            cout << "Salary: " << salary << endl;
+        }
+
+        // Destructor for Employee class
+        ~Employee() {
+            cout << "Employee destructor called for: " << name << endl;
+        }
+};
+
+int main() {
+    // Creating an Employee object (inheriting from Person)
+    cout << "Creating Employee object...\n";
+    Employee e1("John Doe", 30, 5000);
+    
+    // Displaying Employee details
+    cout << "\nEmployee details:\n";
+    e1.showEmployeeDetails();
+
+    cout << "\nEnd of program\n";
+    return 0;
+}
