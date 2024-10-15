a9039a9a98148eb7eb137a1fcd99026fcd8c6ceb
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-10-15 12:18:44 +0530

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+    private:
+        int age;
+        float salary;
+        string name;
+    public:
+        // Constructor
+        employee(int a, float s, string n) {
+            age = a;
+            salary = s;
+            name = n;
+            cout << "Constructor called for employee: " << name << endl;
+        }
+        
+        // Method to display employee details
+        void show() {
+            cout << "Name of employee: " << name << endl;
+            cout << "Salary of employee: " << salary << endl;
+            cout << "Age of employee: " << age << endl;
+        }
+
+        // Destructor
+        ~employee() {
+            cout << "Destructor called for employee: " << name << endl;
+        }
+};
+
+int main() {
+    // Object 1 creation
+    cout << "Creating object 1\n";
+    employee e1(18, 2500, "RAJ");
+    e1.show();
+
+    // Object 2 creation
+    cout << "\nCreating object 2\n";
+    employee e2(22, 3500, "ANIL");
+    e2.show();
+
+    cout << "\nEnd of main function\n";
+    return 0;
+}
