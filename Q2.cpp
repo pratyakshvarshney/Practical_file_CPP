8d35cec72c7bb66688319d26a7f482711f3cbb97
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-10-15 12:08:34 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Book{
+    private: 
+        string title;
+    public:
+        Book(string t){
+            title=t;
+        }
+        Book(Book &t){
+            title=t.title;
+        }
+        void display(){
+            cout<<"Title: "<<title<<endl;
+        }
+};
+
+int main(){
+    Book b1("C++ Programming");
+    Book b2=b1;
+    
+    b1.display();
+    b2.display();
+    
+    return 0;
+}
