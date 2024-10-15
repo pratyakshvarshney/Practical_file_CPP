4539319015170a878d548e82bdd0fcbc5b891c3a
pratyakshvarshney <pratyakshprakhar@gmail.com>
2024-10-15 12:01:41 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Sample{
+    public:
+        Sample(){
+            cout<<"Constructor Called!!"<<endl;
+        }
+        ~Sample(){
+            cout<<"Destructor Called!!"<<endl;
+        }
+};
+
+int main(){
+    Sample obj;
+    
+    return 0;
+}
